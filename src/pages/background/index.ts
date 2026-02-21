ebf9400d5f77746a8c2b775ffea1fe80c761f1da
Yehor Boiar <yb33@st-andrews.ac.uk>
2026-02-21 14:15:35 +0000

    feat: pass score into the chrome context after fetching

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,13 +1,11 @@
-let latestInput = "";
+let latestScore = 0;
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === "INPUT_UPDATED") {
-    latestInput = msg.payload;
+  if (msg.type === "SCORE_UPDATED") {
+    latestScore = msg.payload;
   }
-
-  if (msg.type === "GET_INPUT") {
-    sendResponse(latestInput);
+  if (msg.type === "GET_SCORE") {
+    sendResponse(latestScore);
   }
-
   return true;
 });
