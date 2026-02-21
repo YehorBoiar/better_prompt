6131c2b52023fef2bff8c1c6cbbf4154d9d49c8b
Yehor Boiar <yb33@st-andrews.ac.uk>
2026-02-21 16:43:15 +0000

    feat: add exclamation when score is super high

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -3,6 +3,13 @@ let latestScore = 0;
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "SCORE_UPDATED") {
     latestScore = msg.payload;
+
+    if (latestScore > 60) {
+      chrome.action.setBadgeText({ text: "!" });
+      chrome.action.setBadgeBackgroundColor({ color: "#ef4444" });
+    } else {
+      chrome.action.setBadgeText({ text: "" });
+    }
   }
   if (msg.type === "GET_SCORE") {
     sendResponse(latestScore);
